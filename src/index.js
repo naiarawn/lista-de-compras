7bbce912d87f143c5e2b272f528ed0c97b442cfa
sbcerqra <sarahbia09@gmail.com>
2022-11-14 15:03:51 -0300

    pocketbase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require ('express')
 const app = express();
 app.use(express.json());
-const ControllerProdutos= require ('./controllers/ControllerProdutos')
+const ControllerProdutos= require ('../controllers/ControllerProdutos')
+
 app.post ('/produto/insert', ControllerProdutos.insert)
 app.put ('/produtos/update/:idprodutos', ControllerProdutos.update)
 app.get('/produtos', ControllerProdutos.findAll);
