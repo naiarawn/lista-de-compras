e69fa9ba442a4a7ea6d0770f9af87aa8917cdc87
sbcerqra <sarahbia09@gmail.com>
2022-11-15 19:16:13 -0300

    database connection

--- config/db.js
+++ config/db.js
@@ -1,1 +1,11 @@
-//banco de dados
+const mysql = require ("mysql2/promise"); 
+const DB = async () => {
+    const connection= await mysql.createConnection ({
+        host: "sql10.freesqldatabase.com",
+        user: "sql10573583",
+        password: "cLK9xA6PNn",
+        database: "sql10573583",
+        port: "3306",
+    })
+}
+module.exports = DB();
