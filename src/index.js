6e5eff720f7fa71c3b3e5f232b10b052a58a39aa
sb <84155789+sbcerqra@users.noreply.github.com>
2022-11-27 15:43:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const ControllerProdutos= require ('../controllers/ControllerProdutos')
 app.post ('/produtos/insert', ControllerProdutos.insert)
 app.put ('/produtos/update/:idprodutos', ControllerProdutos.update)
 app.get('/produtos', ControllerProdutos.findAll);
-app.get('/produtos/:id',ControllerProdutos.findById);
-app.delete('/produtos/:id',ControllerProdutos.delete);
+app.get('/produtos/:idprodutos',ControllerProdutos.findById);
+app.delete('/produtos/:idprodutos',ControllerProdutos.delete);
 
 
 app.listen(3333, () => console.log("Server is running."))
