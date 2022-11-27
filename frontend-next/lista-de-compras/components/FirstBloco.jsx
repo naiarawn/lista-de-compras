175d5842f5bfdcf6f2cb8e7b9e0e54658022b4b1
Naiara Nogueira <n4iara@gmail.com>
2022-11-27 19:53:37 -0300

    Montando tela cadastro

--- frontend-next/lista-de-compras/components/FirstBloco.jsx
+++ frontend-next/lista-de-compras/components/FirstBloco.jsx
@@ -4,9 +4,13 @@ export default function FirstBloco(){
   return(
     <div>
       <div className={styles.conteudo}>
-        <h1 className={styles.h1}>Organize todas as suas compras online!</h1>
-        <img src="/DesenhoCompras.svg" alt="Ilustração moça fazendo compras" />
+        <div className={styles.leftBloco}>
+          <h1 className={styles.h1}>Organize todas as suas compras online!</h1>
+          <img className = {styles.bola} src="/Bola.svg" alt="Ilustração moça fazendo compras" />
+        </div>
+        <img src="/DesenhoComprasBolas.svg" alt="Ilustração moça fazendo compras" />
       </div>
+
     </div>
   )
 }
--- frontend-next/lista-de-compras/components/Formulario.jsx
+++ frontend-next/lista-de-compras/components/Formulario.jsx
@@ -2,17 +2,31 @@ import styles from '../styles/Formulario.module.css'
 
 export default function Formulario(){
   return(
-  <>
+  <div className={styles.container}>
     <form action="" className={styles.container}>
-      <label htmlFor="name">Nome do Produto </label>
-      <input type="text" id='name' name='name'/>
-      <label htmlFor="prioridade">Prioridade de compra</label>
-      <input type="text" id='prioridade' name='prioridade'/>      
-      <label htmlFor="price">Preço do Produto</label>
-      <input type="number" id='price' name='price'/>      
-      <label htmlFor="description">Descrição </label>
-      <input type="text" id='description'  name ='description'/>
+      <div className={styles.inputBox}>
+        <label htmlFor="name">Nome do Produto </label>
+        <input type="text" id='name' name='name'/>
+      </div>
+      <div className={styles.agrupa}>
+        <div className={styles.inputBox}>
+          <label htmlFor="priority">Prioridade de compra</label>
+          <input type="text" id='priority' name='priority'/>     
+        </div>
+        <div className={styles.inputBox}>
+          <label htmlFor="price">Preço do Produto</label>
+          <input type="number" id='price' name='price'/>  
+        </div> 
+      </div>
+      <div className={styles.inputBox}>
+        <label htmlFor="whereToBuy">Onde comprar</label>
+        <input type="text" id='whereToBuy' name='whereToBuy'/>  
+      </div> 
+      <div className={styles.inputBox}>
+        <label htmlFor="description">Descrição </label>
+        <input type="text" id='description'  name ='description'/>
+      </div>    
     </form>
-  </>  
+  </div>  
   )
 }
