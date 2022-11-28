cb6a20bd6c07e11b46b060879051e66f0709279d
Naiara Nogueira <n4iara@gmail.com>
2022-11-27 22:39:32 -0300

    Mudando State

--- frontend-next/lista-de-compras/components/Formulario.jsx
+++ frontend-next/lista-de-compras/components/Formulario.jsx
@@ -1,31 +1,51 @@
+import { useState } from 'react'
 import styles from '../styles/Formulario.module.css'
 
 export default function Formulario(){
+
+  const [dataForm, setDataForm] = useState({
+    name: '',
+    priority: '',
+    price: '',
+    whereToBuy: '',
+    description: ''
+  })
+  
+  const onChangeInput = e => setDataForm({...dataForm, [e.target.name]: e.target.value});
+
+  const sendData = async e => {
+    e.preventDefault()
+    console.log(dataForm.price)
+
+    
+  }
+
   return(
   <div className={styles.container}>
-    <form action="" className={styles.container}>
+    <form onSubmit={sendData} action="" className={styles.container}>
       <div className={styles.inputBox}>
         <label htmlFor="name">Nome do Produto </label>
-        <input type="text" id='name' name='name'/>
+        <input type="text" id='name' name='name'onChange={onChangeInput} value={dataForm.name}/>
       </div>
       <div className={styles.agrupa}>
         <div className={styles.inputBox}>
           <label htmlFor="priority">Prioridade de compra</label>
-          <input type="text" id='priority' name='priority'/>     
+          <input type="text" id='priority' name='priority'onChange={onChangeInput} value={dataForm.priority}/>     
         </div>
         <div className={styles.inputBox}>
           <label htmlFor="price">Preço do Produto</label>
-          <input type="number" id='price' name='price'/>  
+          <input type="number" id='price' name='price'onChange={onChangeInput} value={dataForm.price}/>  
         </div> 
       </div>
       <div className={styles.inputBox}>
         <label htmlFor="whereToBuy">Onde comprar</label>
-        <input type="text" id='whereToBuy' name='whereToBuy'/>  
+        <input type="text" id='whereToBuy' name='whereToBuy'onChange={onChangeInput} value={dataForm.whereToBuy}/>  
       </div> 
       <div className={styles.inputBox}>
         <label htmlFor="description">Descrição </label>
-        <input type="text" id='description'  name ='description'/>
-      </div>    
+        <input type="text" id='description'  name ='description'onChange={onChangeInput} value={dataForm.description}/>
+      </div>
+    <button type='submit'>Enviar</button>
     </form>
   </div>  
   )
