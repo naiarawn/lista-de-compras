0e9910d51b9e9756bf42918f6842acc0db93dbef
sbcerqra <sarahbia09@gmail.com>
2022-12-02 13:13:50 -0300

    img responsive nd form fix

--- frontend-next/lista-de-compras/components/ListaProdutos.jsx
+++ frontend-next/lista-de-compras/components/ListaProdutos.jsx
@@ -5,21 +5,39 @@ import SemProduto from './SemProduto';
 
 export default function ListaProdutos(){
 
+  const URL = "http://localhost:3333/produtos/"
   const [data, setData] = useState()
 
   useEffect(() => {
     async function getData(){
-      const response = await axios.get("http://localhost:3333/produtos/")
+      const response = await axios.get(URL)
       setData(response.data)
     }
     getData()
   }, [])
  
+
+  // function deletePost() {
+  //   axios
+  //     .delete("/produtos/:idprodutos")
+  //     .then(() => {
+  //       alert("Post deleted!");
+  //       setData(null)
+  //     });
+  // }
+
+  // const deletePost = async (data) => {
+  //   await axios.delete(URL+ "/:" + data.idprodutos);
+  //   setPosts(posts.filter((p) => p.idprodutos !== data.idprodutos));
+  // };
+
+
+
   return(
     <div>
       
       {data ? data.map((data, i) =>{
-        return (<CadaProduto title={data.name} key={i}/>)
+        return (<CadaProduto title={data.name} key={i} click={() => deletePost(i)}/>)
       })
       : <SemProduto />}
       
