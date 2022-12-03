8234c8ac02d10449d9787d74670d73166d24913b
sbcerqra <sarahbia09@gmail.com>
2022-12-03 03:15:22 -0300

    modal style done

--- frontend-next/lista-de-compras/components/ListaProdutos.jsx
+++ frontend-next/lista-de-compras/components/ListaProdutos.jsx
@@ -1,56 +1,59 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import CadaProduto from '../components/CadaProduto'
-import Modal from './Modal';
-import SemProduto from './SemProduto';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CadaProduto from "../components/CadaProduto";
+import Modal from "./Modal";
+import SemProduto from "./SemProduto";
 
-export default function ListaProdutos(){
-
-  const URL = "http://localhost:3333/produtos/"
-  const [data, setData] = useState([])
+export default function ListaProdutos() {
+  const URL = "http://localhost:3333/produtos/";
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    getData()
-  }, [])
- 
-  async function getData(){
-    const response = await axios.get(URL)
-    setData(response.data)
+    getData();
+  }, []);
+
+  async function getData() {
+    const response = await axios.get(URL);
+    setData(response.data);
   }
 
-    function deleteProduct(idprodutos) {
-      axios
-        .delete(`http://localhost:3333/produtos/${idprodutos}`)
-        .then(() => {
-          alert("Produto removido!");
-          getData()
-        });
-    }
+  function deleteProduct(idprodutos) {
+    axios.delete(`http://localhost:3333/produtos/${idprodutos}`).then(() => {
+      alert("Produto removido!");
+      getData();
+    });
+  }
 
-    function infoProduct(idprodutos) {
-      axios
-        .get(`http://localhost:3333/produtos/${idprodutos}`)
-        .then((res) => {
-          alert(res.data.name);
-        });
-    }
-    
-    const [show, setShow] = useState(false)
+  function infoProduct(idprodutos) {
+    axios.get(`http://localhost:3333/produtos/${idprodutos}`).then((res) => {
+      alert(res.data.name);
+    });
+  }
 
-  return(
+  const [show, setShow] = useState(false);
+
+  return (
     <div>
-      
-      {!!data.length ? data.map((data, i) =>{
-        return (<CadaProduto title={data.name} key={i} click={() => {deleteProduct(data.idprodutos)}} info={()=> setShow(true)}/>)
-      })
-      : (<SemProduto />)}
+      {!!data.length ? (
+        data.map((data, i) => {
+          return (
+            <CadaProduto
+              title={data.name}
+              key={i}
+              click={() => {
+                deleteProduct(data.idprodutos);
+              }}
+              info={() => setShow(true)}
+            />
+          );
+        })
+      ) : (
+        <SemProduto />
+      )}
 
-{/* info={()  => {infoProduct(data.idprodutos)}} */}
+      {/* info={()  => {infoProduct(data.idprodutos)}} */}
 
-      <Modal show={show}/>
-
-
+      <Modal show={show} />
     </div>
-  )
-
+  );
 }
